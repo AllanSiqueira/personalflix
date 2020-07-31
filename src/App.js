4b277542d532a6e42553a92b745cd683852d085a
AllanSiqueira <allan.siqueira@rebase.com.br>
2020-07-31 00:11:39 -0300

    Setup inicial do projeto
    Setup inicial do projeto Imersao React da Alura

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import dadosIniciais from './data/dados_iniciais.json';
+import Menu from './components/Menu'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+        url={dadosIniciais.categorias[0].videos.shift().url}
+      />
+
+      {
+        dadosIniciais.categorias.map(categoria => <Carousel category={categoria} />)
+
+      }
+      <Carousel
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Footer />
     </div>
   );
 }
